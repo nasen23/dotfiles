622528e94f3373a67b680f77ef0151dfe1a1f93f
Sen Na <1439468790@qq.com>
2020-01-31 13:10:06 +0800

    Change emacs theme

--- doom.d/+ui.el
+++ doom.d/+ui.el
@@ -15,7 +15,7 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic nil)
-  (load-theme 'doom-nord-light t)
+  (load-theme 'doom-one-light t)
   (setq doom-themes-treemacs-theme "doom-colors")
   (doom-themes-treemacs-config)
   (doom-themes-org-config)
