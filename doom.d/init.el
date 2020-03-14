773d955b8bc7c46c2134cc56970417f3d55d84ae
Sen Na <1439468790@qq.com>
2020-03-14 22:58:43 +0800

    Use meghanadad for java

--- doom.d/init.el
+++ doom.d/init.el
@@ -121,6 +121,7 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        (java
+        +meghanada
         +lsp) ; the poster child for carpal tunnel syndrome
        (javascript
         +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
