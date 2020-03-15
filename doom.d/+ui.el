d68e98d7008f6010101f5fe0373d20f57d19925f
Sen Na <1439468790@qq.com>
2020-03-16 00:53:45 +0800

    Disable line number for emacs

--- doom.d/+ui.el
+++ doom.d/+ui.el
@@ -4,6 +4,8 @@
 (setq doom-font (font-spec :family "LiterationMono NF" :size 18)
       doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 16)
       doom-unicode-font (font-spec :family "WenQuanYi Micro Hei" :size 16))
+;; disable line numbers at all
+(setq display-line-numbers-type nil)
 
 (after! doom-modeline
   (setq doom-modeline-major-mode-icon t))
