52e4016b1a706c8baeaefd56c868daa8a64ab676
Sen Na <1439468790@qq.com>
2020-03-26 13:15:27 +0800

    Stop using ccls highlight
    Way too fancy...

--- doom.d/+cc.el
+++ doom.d/+cc.el
@@ -1,9 +1,5 @@
 ;;; ~/.dotfiles/doom.d/+cc.el -*- lexical-binding: t; -*-
 
 
-(use-package! ccls
-  :after cc
-  :config
-  (setq ccls-executable "/usr/bin/ccls")
-  (setq ccls-sem-highlight-method 'font-lock)
-  (ccls-use-default-rainbow-sem-highlight))
+(after! ccls
+  (setq ccls-executable "/usr/bin/ccls"))
