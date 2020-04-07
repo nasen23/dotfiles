71bf9a5580f2d120ead7c975586ec7a7179f8a87
Sen Na <1439468790@qq.com>
2020-04-07 09:55:44 +0800

    Properly set cc-style

--- doom.d/+cc.el
+++ doom.d/+cc.el
@@ -3,3 +3,10 @@
 
 (after! ccls
   (setq ccls-executable "/usr/bin/ccls"))
+
+(after! cc-mode
+  (setq c-basic-offset 2)
+  (c-set-offset 'innamespace '-)
+  (c-set-offset 'access-label '--)
+  (c-set-offset 'inclass '+))
+
