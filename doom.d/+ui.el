34d6148605ecc5effe7adff619fcb577cc55de4b
Sen Na <i@nasen.xyz>
2020-05-16 22:02:45 +0800

    Change emacs theme

--- doom.d/+ui.el
+++ doom.d/+ui.el
@@ -11,7 +11,7 @@
 (after! doom-modeline
   (setq doom-modeline-major-mode-icon t))
 
-(setq doom-theme 'doom-snazzy)
+(setq doom-theme 'doom-horizon)
 
 ;; leave some space on left and right border
 ;; (define-globalized-minor-mode global-fringe-mode fringe-mode
