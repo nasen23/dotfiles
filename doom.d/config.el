2a064106cb96ae547d5bdada195a67c16d50b928
Sen Na <i@nasen.xyz>
2020-07-09 00:15:27 +0800

    Update emacs conf

--- doom.d/config.el
+++ doom.d/config.el
@@ -83,9 +83,3 @@
      ("http" . "localhost:8888")
      ("https" . "localhost:8888")))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(italic ((t (:slant normal)))))
