b5ea57ffb1bac50026cd93517a7a60383e7eaada
Sen Na <nasen2582995949@163.com>
2023-12-24 14:19:30 +0800

    Update config

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,1 +1,45 @@
- (setq package-enable-at-startup nil)
+;;; early-init.el --- Early initialization. -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+;; Emacs 27 introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+;;
+
+;;; Code:
+
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
+;; packages are compiled ahead-of-time when they are installed and site files
+;; are compiled when gccemacs is installed.
+(setq native-comp-deferred-compilation nil ;; obsolete since 29.1
+      native-comp-jit-compilation nil)
+
+;; Package initialize occurs automatically, before `user-init-file' is
+;; loaded, but after `early-init-file'. We handle package
+;; initialization, so we must prevent Emacs from doing it early!
+(setq package-enable-at-startup nil)
+
+;; `use-package' is builtin since 29.
+;; It must be set before loading `use-package'.
+(setq use-package-enable-imenu-support t)
+
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Inhibit resizing frame
+(setq frame-inhibit-implied-resize t)
+
+;; Faster to disable these here (before they've been initialized)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(when (featurep 'ns)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
+(setq-default mode-line-format nil)
+
+;;; early-init.el ends here
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,5 +1,17 @@
+;;; init.el --- My Emacs configuration.	-*- lexical-binding: t no-byte-compile: t -*-
+
+;;; Commentary:
+;;
+;; A sensible Emacs config with meow modal editing.
+;;
+
+
+;;; Code:
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
+
+;;; init.el ends here
 (put 'dired-find-alternate-file 'disabled nil)
